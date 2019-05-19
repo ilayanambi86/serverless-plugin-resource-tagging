220f721eac376753103bc71e755af9b5315a2205
Ilayanambi <ilayanambi_ponramu@comcast.com>
2019-05-19 01:16:55 -0400

    added api gateway support

--- README.md
+++ README.md
@@ -3,6 +3,10 @@
 Serverless stackTags will update the tags for all the resources that support tagging. But the issue is it will update once during create. If you update the tag values after deployment, it wont reflect in next deployment.
 We have to remove the stack and redeploy to get the new tags reflect. This plugin will solve that issue for AWS.
 
+#### Note: 
+ - This plugin is only for AWS.
+ - This plugin will support APIGateway stage tags even if stage is not configured in serverless.yml and clouformation created one. 
+
 ### Using this pluging
 ```
 npm install serverless-plugin-resource-tagging
@@ -12,7 +16,7 @@ npm install serverless-plugin-resource-tagging
 ```
 provider:
     name: XXX
-    resourceTags:
+    stackTags:
         Tag1: "Tag1 value"
         Tag2: "Tag2 value"
 plugins:
@@ -21,10 +25,11 @@ plugins:
     
 ### Suported AWS resources
 ```
-"AWS::Lambda::Function",
-"AWS::SQS::Queue",
-"AWS::Kinesis::Stream",
-"AWS::DynamoDB::Table",
-"AWS::S3::Bucket",
-"AWS::ApiGateway::Stage"
+AWS::Lambda::Function
+AWS::SQS::Queue
+AWS::Kinesis::Stream
+AWS::DynamoDB::Table
+AWS::S3::Bucket
+AWS::ApiGateway::Stage
+AWS::CloudFront::Distribution
 ```
--- index.js
+++ index.js
@@ -1,60 +1,106 @@
 'use strict';
 
+const _ = require('underscore');
 
 class ServerlessPlugin {
   constructor(serverless, options) {
     this.serverless = serverless;
     this.options = options;
+    this.provider = serverless ? serverless.getProvider('aws') : null;
+    this.service = serverless.service;
+    this.stage = null;
+    this.region = null;
+    this.isApiGatewayStageAvailableInTemplate = false;
     this.supportedTypes = [
       "AWS::Lambda::Function",
       "AWS::SQS::Queue",
       "AWS::Kinesis::Stream",
       "AWS::DynamoDB::Table",
       "AWS::S3::Bucket",
-      "AWS::ApiGateway::Stage"
-    ]
+      "AWS::ApiGateway::Stage",
+      "AWS::CloudFront::Distribution"
+    ];
+
+    if (!this.provider) {
+      throw new Error('This plugin must be used with AWS');
+    }
+
     this.hooks = {
-      'after:deploy:deploy': this.addTagsToResource.bind(this),
-      'after:aws:package:finalize:mergeCustomProviderResources': this.addTagsToResource.bind(this)
+      'deploy:finalize': this._addAPIGatewayStageTags.bind(this),
+      'after:deploy:deploy': this._addTagsToResource.bind(this),
+      'after:aws:package:finalize:mergeCustomProviderResources': this._addTagsToResource.bind(this)
     };
   }
 
-  addTagsToResource() {
-    var resourceTags = [];
+  _addTagsToResource() {
+    var stackTags = [];
     var self = this;
     const template = this.serverless.service.provider.compiledCloudFormationTemplate;
 
-    var stage = this.serverless.service.provider.stage;
+
+    this.stage = this.serverless.service.provider.stage;
     if (this.serverless.variables.options.stage) {
-      stage = this.serverless.variables.options.stage;
+      this.stage = this.serverless.variables.options.stage;
     }
 
-    if (typeof this.serverless.service.provider.resourceTags === 'object') {
-      var tags = this.serverless.service.provider.resourceTags
+    this.region = this.serverless.service.provider.region;
+    if (this.serverless.variables.options.region) {
+      this.region = this.serverless.variables.options.region;
+    }
+
+    if (typeof this.serverless.service.provider.stackTags === 'object') {
+      var tags = this.serverless.service.provider.stackTags
       Object.keys(tags).forEach(function (key) {
-        resourceTags.push({ "Key": key, "Value": tags[key] })
+        stackTags.push({ "Key": key, "Value": tags[key] })
       });
     }
 
     Object.keys(template.Resources).forEach(function (key) {
       var resourceType = template.Resources[key]['Type']
-      if ((self.supportedTypes.indexOf(resourceType) !== -1) && Array.isArray(resourceTags) && resourceTags.length > 0) {
+      if ((self.supportedTypes.indexOf(resourceType) !== -1) && Array.isArray(stackTags) && stackTags.length > 0) {
         if (template.Resources[key]['Properties']) {
           var tags = template.Resources[key]['Properties']['Tags']
           if (tags) {
-            template.Resources[key]['Properties']['Tags'] = tags.concat(resourceTags.filter(obj => (self.getTagNames(tags).indexOf(obj["Key"]) === -1)))
+            template.Resources[key]['Properties']['Tags'] = tags.concat(stackTags.filter(obj => (self._getTagNames(tags).indexOf(obj["Key"]) === -1)))
           } else {
-            template.Resources[key]['Properties']['Tags'] = resourceTags
+            template.Resources[key]['Properties']['Tags'] = stackTags
           }
         } else {
           self.serverless.cli.log('Properties not available for ' + resourceType);
         }
       }
+
+      //Flag to avoid _addAPIGatewayStageTags() call if stage config is available in serverless.yml
+      if (resourceType === "AWS::ApiGateway::Stage") {
+        self.isApiGatewayStageAvailableInTemplate = true;
+      }
     });
-    self.serverless.cli.log('Updated resource tags..');
+    self.serverless.cli.log('Updated AWS resource tags..');
   }
 
-  getTagNames(srcArray) {
+  _addAPIGatewayStageTags() {
+    var self = this;
+    var stackName = this.provider.naming.getStackName();
+    if (!self.isApiGatewayStageAvailableInTemplate) {
+      return this.provider.request('CloudFormation', 'describeStackResources', { StackName: stackName })
+        .then(function (resp) {
+          var promiseStack = [];
+          _.each(_.filter(resp.StackResources, resource => resource.ResourceType === 'AWS::ApiGateway::RestApi'), function (resource) {
+            var apiStageParams = {
+              resourceArn: 'arn:aws:apigateway:' + self.region + '::/restapis/' + resource.PhysicalResourceId + '/stages/' + self.stage,
+              tags: self.service.provider.stackTags
+            };
+            promiseStack.push(self.provider.request('APIGateway', 'tagResource', apiStageParams))
+          });
+          return Promise.all(promiseStack).then(resp => self.serverless.cli.log('Updated APIGateway resource tags..'));
+        });
+    } else {
+      self.serverless.cli.log('APIGateway stage already available in serverless.yml. Tag update skipped.');
+      return null;
+    }
+  }
+
+  _getTagNames(srcArray) {
     var tagNames = []
     srcArray.forEach(function (element) {
       tagNames.push(element["Key"])
